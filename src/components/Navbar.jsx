bfe42eabeb1cce78552d3e563a5eb04b92fc2643
oMoFoLaYaN <nathanadeosun2003@gmail.com>
2023-03-16 15:20:10 +0100

    go

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { useState } from 'react';
 import { navLinks } from "../constants";
 import { layout } from '../style';
+import Theme from './Theme';
 
 
 const Navbar = () => {
   const [active, setActive] = useState("Home");
   const [toggle, setToggle] = useState(false);
+  const close = "ri-close-line";
+  const menu = "ri-function-line";
 
 
 return (
@@ -15,30 +18,50 @@ return (
     <a href="#" className="font-medium text-[color:var(--white-color)] bg-slate-600">
       Cruise
     </a>
-      <ul className="flex flex-col gap-y-10 md:flex-row md:gap-x-16">
+      <ul className="sm:flex hidden gap-y-10 flex-row sm:gap-x-14 md:gap-x-16">
       {navLinks.map((nav, index) => (
-        <li key={nav.id} className="relative md:flex md:gap-x-4">
-          <a href={`#${nav.id}`} className="text-[color:var(--text-color-light)] 
-          font-medium capitalize hover:text-[color:var(--text-color)] 
-          md:text-[color:var(--white-color)] md:hover:text-[color:var(--white-color)] 
-          relative  before:content-[''] before:absolute before:bg-[color:var(--title-color)] 
-          before:w-full before:h-0.5 before:-bottom-3 before:left-0 
-          md:before:bg-[color:var(--white-color)];">
+        <li key={nav.id} className="relative md:flex md:gap-x-4 text-[color:var(--text-color-light)] 
+        font-medium capitalize hover:text-[color:var(--text-color)] 
+        md:text-[color:var(--white-color)] md:hover:text-[color:var(--white-color)]">
+
+          <a href={`#${nav.id}`} className={`${
+              active === nav.title ? "text-white" : "text-red-500"
+            } ${index === navLinks.length - 1 ? "mr-0" : "mr-2"}`}
+            onClick={() => setActive(nav.title)}
+            >
           {nav.title}
           </a>
-        </li>
+          </li>
         ))}
+        <Theme/>
       </ul>
       
-       <i className="ri-close-line nav__close" id="nav-close" />
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <i className={`w-[28px] h-[28px] object-contain ${toggle ? close : menu}`}
+          onClick={() => setToggle(!toggle)}
+        />
 
-    <div className="nav__dark">
-        <i className="ri-moon-line change-theme" id="theme-button" />
-    </div>
-
-    <div className="nav__toggle" id="nav-toggle">
-      <i className="ri-function-line" />
-    </div>
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-teal-300 absolute top-0 right-0 w-9/12 h-screen -z-10 sidebar`}
+        >
+          <ul className="list-none flex justify-start items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-base ${
+                  active === nav.title ? "text-white" : "text-amber-900"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+            <Theme/>
+          </ul>
+        </div>
+      </div>
 
   </nav>
 </header>
@@ -46,3 +69,14 @@ return (
 }
 
 export default Navbar
+
+
+{/* <i className="ri-close-line nav__close" id="nav-close" />
+
+<div className="nav__dark">
+    <i className="ri-moon-line change-theme" id="theme-button" />
+</div>
+
+<div className="nav__toggle" id="nav-toggle">
+  <i className="ri-function-line" />
+</div> */}
---
+++ src/components/Theme.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Theme = () => {
+  return (
+<div className="nav__dark">
+    <i className="ri-moon-line change-theme text-black" id="theme-button" />
+</div>
+  )
+}
+
+export default Theme
