03f986b885719a61d78d77a1e9c56bd4197483f3
oMoFoLaYaN <nathanadeosun2003@gmail.com>
2023-03-16 20:13:40 +0100

    issues

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { home1, home2 } from "../assets";
 import { Social } from "../components";
 import Button from './Button';
+import styles from '../style';
 
 
 const Hero = () => (
     <section className="home" id="home">
-  <img src={home1} alt="" className="home__img" />
-  <div className="home__container container grid gap-6">
+  <img src={home1} alt="" className="absolute w-full h-screen object-cover object-[83%] left-0 top-0" />
+  <div className={`relative h-[calc(100vh-3rem)] content-center gap-y-12 md:h-screen md:grid-rows-[1.8fr_0.5fr] lg:grid-rows-[2fr_0.5fr] ${styles.container} grid gap-6`}>
     <div className="home__data">
       <span className="home__data-subtitle">Discover your place</span>
       <h1 className="home__data-title ">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,10 +15,10 @@ const Navbar = () => {
 return (
     <header className="w-full fixed z-[100] bg-transparent left-0 top-0" id="header">
   <nav className={`h-12 flex justify-between items-center md:h-[18] ${layout.container}`}>
-    <a href="#" className="font-medium text-[color:var(--white-color)] bg-slate-600">
+    <a href="#" className="font-medium text-[color:var(--white-color)]">
       Cruise
     </a>
-      <ul className="sm:flex hidden gap-y-10 flex-row sm:gap-x-14 md:gap-x-16">
+      <ul className="md:flex hidden gap-y-10 flex-row md:gap-x-16">
       {navLinks.map((nav, index) => (
         <li key={nav.id} className="relative md:flex md:gap-x-4 text-[color:var(--text-color-light)] 
         font-medium capitalize hover:text-[color:var(--text-color)] 
@@ -36,8 +36,8 @@ return (
         <Theme/>
       </ul>
       
-      <div className="sm:hidden flex flex-1 justify-end items-center">
-        <i className={`w-[28px] h-[28px] object-contain ${toggle ? close : menu}`}
+      <div className="md:hidden flex flex-1 justify-end items-center">
+        <i className={`w-7 h-7 text-xl object-contain ${toggle ? close : menu}`}
           onClick={() => setToggle(!toggle)}
         />
 
@@ -69,14 +69,3 @@ return (
 }
 
 export default Navbar
-
-
-{/* <i className="ri-close-line nav__close" id="nav-close" />
-
-<div className="nav__dark">
-    <i className="ri-moon-line change-theme" id="theme-button" />
-</div>
-
-<div className="nav__toggle" id="nav-toggle">
-  <i className="ri-function-line" />
-</div> */}
